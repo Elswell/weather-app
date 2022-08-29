3fe7d1ca1a5f4b8d71635f6e173a31d9237156da
Elswell <djem.hasan60@gmail.com>
2022-08-29 15:14:01 +0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a560e45b32mshbc35f5108934104p1df639jsn56d23450dd06",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "57a0342fc2a98ec1c3e1bc155b52c3f7";
